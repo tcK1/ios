e38becf86868d700f86fc2cdc0e397d1e1dc1229
Maciej Łodygowski <tiruriru95@gmail.com>
2025-04-01 18:38:23 +0200

    fix: correct name for working directory variable (#2)
    Co-authored-by: maciej.lodygowski <maciej.lodygowski@callstack.com>

--- .github/actions/rnef-native-fingerprint/index.mjs
+++ .github/actions/rnef-native-fingerprint/index.mjs
@@ -7,7 +7,7 @@ const ALLOWED_PLATFORMS = ["android", "ios"];
 
 async function run() {
   const platform = core.getInput("platform");
-  const workingDirectory = core.getInput("working-directory");
+  const workingDirectory = core.getInput("working_directory");
   if (!ALLOWED_PLATFORMS.includes(platform)) {
     throw new Error(`Invalid platform: ${platform}`);
   }
